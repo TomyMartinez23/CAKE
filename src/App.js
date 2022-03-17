a014af5271f06dc6a551246b7be19ab09b477fe0
Tomy <martineztomy74@gmail.com>
2022-03-16 22:14:08 -0300

    Clase 3

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
 import './App.css';
+import Navbar from "./Components/Navbar";
 
 function App() {
+
   return (
-    <div className="App">
-      <header >
-        <img src="logo192.png" className='img-header'/>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-logo">
+      <Navbar />
     </div>
   );
 }
---
+++ src/Componets/Navbar.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+function Navbar(){
+    return(
+        <header className='main-header'>
+        <img src="logo192.png" className='img-header'/>
+        <ul className='navbar'>
+          <li><button>Inicio</button></li>
+          <li><button>Productos</button></li>
+          <li><button>Nosotros</button></li>
+          <li><button>Contacto</button></li>
+        </ul>
+      </header>
+    )
+}
+
+export default Navbar;
+
+
+
