678c3e5531682e6cda889f93f01f5a8dc9972e32
Tomy <martineztomy74@gmail.com>
2022-03-22 19:16:27 -0300

    .

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,11 +1,12 @@
 import React from "react"
 import Button from '@mui/material/Button';
+import logo from "./imgProyecto/logo.png"
 
 
-function Navbar() {
+function Navbar(props) {
     return(
         <header className='main-header'>
-        <img src="./imgProyecto/Logo2.jpg" className='Container-logo'/>
+        <img src= {logo} className='Container-logo'/>
         <ul className='navbar'>
           <li><Button variant="contained">Inicio</Button></li>
           <li><Button variant="contained">Productos</Button></li>
